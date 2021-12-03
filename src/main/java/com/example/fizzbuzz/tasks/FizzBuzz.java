55a18073397f1685740eb4a98b9e2965cc80fb76
Mbagou <gilles.boussengui@gmail.com>
2021-12-03 17:14:49 +0100

    Create a bunch of test scenarios to show output

--- src/main/java/com/example/fizzbuzz/tasks/FizzBuzz.java
+++ src/main/java/com/example/fizzbuzz/tasks/FizzBuzz.java
@@ -2,7 +2,21 @@ package com.example.fizzbuzz.tasks;
 
 public class FizzBuzz {
 
-  public String generate(int i) {
-    return ( i % 15 == 0)? "FizzBuzz" : (i % 3 == 0)? "Fizz" : (i % 5 == 0) ? "Buzz": String.valueOf(i);
+  public String generate(int debut,int fin) {
+    return checkSetOfNumbers(debut, fin);
+  }
+
+  private String checkSetOfNumbers(int begin,int end){
+    StringBuilder resultSet = new StringBuilder();
+    while ( begin <= end){
+      resultSet.append(checkFizBuz(begin++));
+    }
+    return resultSet.toString();
+  }
+
+  private String checkFizBuz(int number){
+    return (number % 15 == 0) ? "FizzBuzz" :
+      (number % 3 == 0) ? "Fizz" :
+        (number % 5 == 0) ? "Buzz" : String.valueOf(number);
   }
 }
--- src/test/java/com/example/fizzbuzz/tasks/FizzBuzzTest.java
+++ src/test/java/com/example/fizzbuzz/tasks/FizzBuzzTest.java
@@ -18,9 +18,50 @@ class FizzBuzzTest {
     // Given 1
     // When call function
 
-    String result = fizzBuzz.generate(1);
+    String result = fizzBuzz.generate(1,1);
 
     // Then should return "1".
     assertEquals("1", result);
   }
+  @Test
+  public void shouldReturn_Two_Given_2_As_input(){
+
+     // Given 2
+     // When Calling generate method
+       String result = fizzBuzz.generate(2,2);
+     // Then It should return "2"
+      assertEquals("2", result);
+  }
+  @Test
+  public void shouldReturnFizzIfNumberIs3() {
+    assertEquals("Fizz", fizzBuzz.generate(3, 3));
+  }
+
+  @Test
+  public void shouldReturnFizzIfNumberIs6() {
+    assertEquals("Fizz", fizzBuzz.generate(6, 6).trim());
+  }
+
+  @Test
+  public void shouldReturnBuzzIfNumberIs5() {
+    assertEquals("Buzz", fizzBuzz.generate(5, 5).trim());
+  }
+
+  @Test
+  public void shouldReturnFizzBuzzIfNumberIs15() {
+    assertEquals("FizzBuzz", fizzBuzz.generate(15, 15).trim());
+
+  }
+
+  @Test
+  public void shouldReturnFizzBuzzIfNumberIs30() {
+    assertEquals("FizzBuzz", fizzBuzz.generate(30, 30).trim());
+  }
+
+  @Test
+  public void shouldReturn12IframeAre1And2() {
+    assertEquals("12", fizzBuzz.generate(1, 2).trim());
+  }
+
+
 }
